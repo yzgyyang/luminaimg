19465582338a433c6590a8189ab3e9255b20a57e
Guangyuan Yang <yzgyyang@outlook.com>
2019-06-09 18:58:43 -0400

    add: dev: add basic auth pages and db config

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object('config')
 
+app.secret_key = app.config["APP_SECRET_KEY"]
+
 db = SQLAlchemy(app)
 
 from app import views
--- app/models.py
+++ app/models.py
@@ -7,6 +7,10 @@ class Users(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(16), unique=True)
+    email = db.Column(db.String(32), unique=True)
+    password = db.Column(db.String(128))
+    name = db.Column(db.String(32))
+    bio = db.Column(db.String(512))
     is_activated = db.Column(db.Boolean,
                              nullable=False,
                              server_default=expression.true())

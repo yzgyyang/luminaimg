475fa213ebbd4f06ea2a5942018f518c312cf848
Guangyuan Yang <yzgyyang@outlook.com>
2019-06-09 01:11:22 -0400

    new: dev: add basic s3 functionality

--- config.py
+++ config.py
@@ -8,3 +8,8 @@ DEBUG = True
 # Define the database
 SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://luminauser:luminapassword@localhost:3306/luminadb'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+# Amazon S3 config from env
+S3_BUCKET = os.environ.get("S3_BUCKET")
+S3_KEY = os.environ.get("S3_KEY")
+S3_SECRET = os.environ.get("S3_SECRET")

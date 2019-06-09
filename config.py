18c3b34678684c4fccbe9305bc767de08a15caa4
Guangyuan Yang <yzgyyang@outlook.com>
2019-06-09 17:35:46 -0400

    chg: dev: make db config available via env var

--- config.py
+++ config.py
@@ -5,8 +5,19 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 # Enable the development environment
 DEBUG = True
 
+# DB config from env
+DB_USER = os.environ.get("DB_USER", "luminauser")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "luminapassword")
+DB_URI = os.environ.get("DB_URI", "localhost:3306")
+DB_NAME = os.environ.get("DB_NAME", "luminadb")
+
 # Define the database
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://luminauser:luminapassword@localhost:3306/luminadb'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://{db_user}:{db_password}@{db_uri}/{db_name}'.format(
+    db_user=DB_USER,
+    db_password=DB_PASSWORD,
+    db_uri=DB_URI,
+    db_name=DB_NAME,
+)
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 # Amazon S3 config from env
